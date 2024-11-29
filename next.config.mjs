b44a535543bc3b45424e06fbf9f443ecb8cccd18
maplexiao <maplexiao@tencent.com>
2024-11-29 18:00:45 +0800

    feat(test): 测试 rewrite

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  // 添加以下配置以确保生成所有静态路由
+  trailingSlash: true,
+  images: {
+    unoptimized: true
+  }
 };
 
 export default nextConfig;
