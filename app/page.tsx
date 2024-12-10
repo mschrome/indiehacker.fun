94f5996dbcd54ca431c87a06b8c08ce52276661a
maplexiao <maplexiao@tencent.com>
2024-12-10 17:03:16 +0800

    feat(normal): test headers jpg

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,15 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24">
+      <div className="relative w-64 h-64 mb-8">
+        <Image
+          src="/images/1.jpg"
+          alt="Logo"
+          fill
+          style={{ objectFit: 'contain' }}
+          priority
+        />
+      </div>
       <div className="z-10 items-center justify-between w-full max-w-5xl font-mono text-sm lg:flex">
         <p className="fixed top-0 left-0 flex justify-center w-full pt-8 pb-6 border-b border-gray-300 bg-gradient-to-b from-zinc-200 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
