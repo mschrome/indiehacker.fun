0ced6d35e5d6d8d371f9aa3b29f51a20791a4d94
maplexiao <maplexiao@tencent.com>
2025-05-19 16:16:51 +0800

    feat(normal): test cache

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +18,10 @@ export default function RootLayout({
   console.log('REACT_APP_API_URL:', process.env.REACT_APP_API_URL);
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Script src="/assets/test.js" strategy="afterInteractive" />
+      </body>
     </html>
   );
 }
